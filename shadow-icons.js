94e55b9528cd2b11da40c32596dc0cad07fcff10
brunothesatellite <40961631+brunothesatellite@users.noreply.github.com>
2026-09-22 23:07:35 +0200

    Préparation des icons shadow

---
+++ shadow-icons.js
@@ -0,0 +1,9 @@
+window.DDRedBoxIcons = {
+  clerc:    'icons/shadow/crusader.png',
+  elfe:     'icons/shadow/elf_male.png',
+  guerrier: 'icons/shadow/knight.png',
+  halfelin: 'icons/shadow/halflings.png',
+  mage:     'icons/shadow/wizard.png',
+  nain:     'icons/shadow/dwarf.png',
+  voleur:   'icons/shadow/bandit.png',
+};

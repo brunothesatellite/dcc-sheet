bf0c37c746d3efb4f7460fa732726f63e1ab8de8
brunothesatellite <40961631+brunothesatellite@users.noreply.github.com>
2026-09-23 21:14:40 +0200

    Augmentation durée de session

--- api/auth.php
+++ api/auth.php
@@ -1,5 +1,11 @@
 <?php
 require_once __DIR__ . '/db.php';
+session_set_cookie_params([
+    'lifetime' => 30 * 24 * 3600,
+    'path'     => '/',
+    'httponly'  => true,
+    'samesite'  => 'Lax'
+]);
 session_start();
 
 $input = json_decode(file_get_contents('php://input'), true);
